45111e235fa7e29984f98f83d038fdbb7d0e5c43
Peter Lada <peter@guidebook.com>
2016-04-05 16:33:36 -0700

    move from grunt to gulp

---
+++ compass-config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+
+add_import_path "node_modules/"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
